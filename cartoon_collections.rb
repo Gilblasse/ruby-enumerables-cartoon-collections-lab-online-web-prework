dfb0e0f4e01ea14cb67c44fd6b774d5b9ab8d1e1
Nethelbert <gilblasse@gmail.com>
2019-11-03 23:28:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.collect {|item| array.select{|val| item == val}.reduce}.compact.reduce
+  cheese_types.reduce {|item| array.find{|val| item == val}}
 end
